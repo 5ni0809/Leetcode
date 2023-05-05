0e446c8daa0024354f7cfef084d57698722153c5
5ni0809 <wu5450596@gmail.com>
2023-05-05 16:24:06 +0800

    23 0505

--- Medium/ContainerWithMostWater.cpp
+++ Medium/ContainerWithMostWater.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution
@@ -7,6 +8,29 @@ class Solution
 public:
     int maxArea(vector<int> &height)
     {
-        cout << "*";
+        int area = 0, n = height.size();
+        int i = 0, j = n - 1;
+        while (i < j)
+        {
+            if (height[i] > height[j])
+            {
+                area = max(area, height[j] * (j - i));
+                j--;
+            }
+            else
+            {
+                area = max(area, height[i] * (j - i));
+                i++;
+            }
+        }
+        return area;
     }
 };
+int main(void)
+{
+    Solution s;
+    vector<int> v1 = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    vector<int> v2 = {1, 1};
+    s.maxArea(v1);
+    s.maxArea(v2);
+}
